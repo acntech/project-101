d4f311a489a73f022cd16a5f601e929b1478946c
Ørjan Johansen <orjjoh@vegvesen.no>
2019-08-23 10:33:29 +0200

    fixed integration test for employee

--- backend/web/src/test/java/no/acntech/project101/web/employee/resources/EmployeeResourceIT.java
+++ backend/web/src/test/java/no/acntech/project101/web/employee/resources/EmployeeResourceIT.java
@@ -1,5 +1,25 @@
 package no.acntech.project101.web.employee.resources;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import no.acntech.project101.Project101Application;
 import no.acntech.project101.company.Company;
 import no.acntech.project101.company.service.CompanyService;
@@ -8,28 +28,10 @@ import no.acntech.project101.employee.service.EmployeeService;
 import no.acntech.project101.web.TestUtil;
 import no.acntech.project101.web.company.resources.CompanyDto;
 import no.acntech.project101.web.company.resources.converter.CompanyConverter;
-import no.acntech.project101.web.employee.resources.converter.EmployeeConverter;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.*;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Project101Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -41,20 +43,23 @@ class EmployeeResourceIT {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
-    @SpyBean
+    @Autowired
     private EmployeeService employeeService;
 
-    @SpyBean
+    @Autowired
     private CompanyService companyService;
+
 
     @Test
     void findAll() {
+        final Company acme = companyService.save(new Company("ACME", "123456789"));
         final Employee ken = new Employee("Ken", "Guru", LocalDate.of(1994, 10, 1));
+        ken.setCompany(acme);
         final Employee tor = new Employee("Tor", "Divel", LocalDate.of(1994, 10, 1));
-        CompanyDto companyDto = new CompanyDto(1L, "ACME", "123456789");
-        ken.setCompany(new CompanyConverter().convert(companyDto));
-        tor.setCompany(new CompanyConverter().convert(companyDto));
-        doReturn(Arrays.asList(ken, tor)).when(employeeService).findAll();
+        tor.setCompany(acme);
+
+        employeeService.save(ken);
+        employeeService.save(tor);
 
         ResponseEntity<EmployeeDto[]> response = testRestTemplate.exchange(
                 TestUtil.createURL(port, "/employees"),
@@ -64,25 +69,35 @@ class EmployeeResourceIT {
         );
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         List<EmployeeDto> employees = Arrays.asList(response.getBody());
-        assertThat(employees).hasSize(2);
+
+        assertThat(employees)
+                .isNotEmpty()
+                .extracting(EmployeeDto::getFirstName, EmployeeDto::getLastName, EmployeeDto::getDateOfBirth)
+                .contains(
+                        Tuple.tuple(ken.getFirstName(), ken.getLastName(), ken.getDateOfBirth()),
+                        Tuple.tuple(tor.getFirstName(), tor.getLastName(), tor.getDateOfBirth())
+                );
     }
 
     @Test
     void findById() {
-        final Employee ken = new Employee("Ken", "Guru", LocalDate.of(1994, 10, 1));
-        final CompanyDto companyDto = new CompanyDto(1L, "ACME", "123456789");
+        final Company acme = companyService.save(new Company("ACME", "123456789"));
 
-        doReturn(Optional.of(ken)).when(employeeService).findById(1L);
-        ken.setCompany(new CompanyConverter().convert(companyDto));
+        final Employee ken = new Employee("Ken", "Guru", LocalDate.of(1994, 10, 1));
+        ken.setCompany(acme);
+        final Employee savedKen = employeeService.save(ken);
+
 
         ResponseEntity<EmployeeDto> response = testRestTemplate.exchange(
-                TestUtil.createURL(port, "/employees/1"),
+                TestUtil.createURL(port, "/employees/" + savedKen.getId()),
                 HttpMethod.GET,
                 new HttpEntity<>(null, new HttpHeaders()),
                 EmployeeDto.class
         );
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         EmployeeDto employeeDto = response.getBody();
+
+
         assertThat(employeeDto.getDateOfBirth()).isEqualTo(ken.getDateOfBirth());
         assertThat(employeeDto.getFirstName()).isEqualTo(ken.getFirstName());
         assertThat(employeeDto.getLastName()).isEqualTo(ken.getLastName());
@@ -90,16 +105,11 @@ class EmployeeResourceIT {
 
     @Test
     void createEmployee() {
-        final Employee ken = new Employee("Ken", "Guru", LocalDate.of(1994, 10, 1));
-        final EmployeeDto kenDto = new EmployeeDto(1L, "Ken", "Guru", LocalDate.of(1994, 10, 1), 1L);
-        final CompanyDto companyDto = new CompanyDto(1L, "ACME", "123456789");
-        final Company company = new CompanyConverter().convert(companyDto);
+        final Company acme = companyService.save(new Company("ACME", "123456789"));
 
-        doReturn(ken).when(employeeService).save(ken);
-        doReturn(Optional.of(company)).when(companyService).findById(anyLong());
+        final EmployeeDto kenDto = new EmployeeDto(null, "Ken", "Guru", LocalDate.of(1994, 10, 1), acme.getId());
 
         HttpEntity<EmployeeDto> entity = new HttpEntity<>(kenDto, new HttpHeaders());
-        companyService.save(company);
 
         ResponseEntity response = testRestTemplate.exchange(
                 TestUtil.createURL(port, "/employees/"),
@@ -108,16 +118,18 @@ class EmployeeResourceIT {
                 ResponseEntity.class
         );
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        assertThat(response.getHeaders().get(HttpHeaders.LOCATION).get(0)).contains("/employees/1");
+        assertThat(response.getHeaders().get(HttpHeaders.LOCATION).get(0)).containsPattern("\\/employees\\/\\d+");
     }
 
     @Test
     void deleteEmployee() {
+        final Company acme = companyService.save(new Company("ACME", "123456789"));
         final Employee ken = new Employee("Ken", "Guru", LocalDate.of(1994, 10, 1));
-        doReturn(Optional.of(ken)).when(employeeService).findById(anyLong());
+        ken.setCompany(acme);
+        final Employee savedKen = employeeService.save(ken);
 
         ResponseEntity response = testRestTemplate.exchange(
-                TestUtil.createURL(port, "/employees/1"),
+                TestUtil.createURL(port, "/employees/" + savedKen.getId()),
                 HttpMethod.DELETE,
                 new HttpEntity<>(null, new HttpHeaders()),
                 ResponseEntity.class
