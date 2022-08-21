7af8630bdc1d27b891d34b457a32eee04af392aa
Lars Henrik Nordli <lars.henrik.nordli@gmail.com>
2022-08-21 21:58:39 +0200

    Upgrade dependencies and fix audits (still one vuln remaining). Updated CRA, React, Bootstrap/Reactstrap, React Router. Removed PropTypes.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+);
